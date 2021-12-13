7f93bbc5c59669343d7a8d9b788f0df0376d8075
camilligan89 <67126551+camilligan89@users.noreply.github.com>
2021-12-13 14:57:18 -0500

    style up CSS

--- src/index.js
+++ src/index.js
@@ -37,9 +37,7 @@ function displayForcast() {
       alt=""
       width= "42"/>
   <div class="weather-forecast-temperatures"><span class="weather-forecast-temperature-max">18°</span> <span class="weather-forecast-temperature-min">12°</span></div>
-
-</div>
-</div>
+  </div>
 `;
   });
 
