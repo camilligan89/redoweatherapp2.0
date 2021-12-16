e81a23ddd8d76e5585894d2fb2e4b69c9af38ec3
camilligan89 <67126551+camilligan89@users.noreply.github.com>
2021-12-16 18:41:24 -0500

    change api url on 75 and 107

--- src/index.js
+++ src/index.js
@@ -73,7 +73,6 @@ function displayForecast(response) {
 function getForecast(coordinates) {
   let apiKey = "7fe508d235d0ee4554ec8bb21032d44e";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
-  axios.get(apiUrl).then(displayForecast);
 }
 
 function displayTemperature(response) {
@@ -104,7 +103,7 @@ function displayTemperature(response) {
 
 function search(city) {
   let apiKey = "7fe508d235d0ee4554ec8bb21032d44e";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?q=${city}&appid=${apiKey}&units=imperial`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(displayTemperature);
 }
 
