d94f2b0fc61c566f921b4c93b1dfe6ebcc6d3c66
camilligan89 <67126551+camilligan89@users.noreply.github.com>
2021-12-15 20:57:31 -0500

    correct api coord

--- src/index.js
+++ src/index.js
@@ -104,7 +104,7 @@ function displayTemperature(response) {
 
 function search(city) {
   let apiKey = "7fe508d235d0ee4554ec8bb21032d44e";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?q=${city}&appid=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(displayTemperature);
 }
 
