078b83f744b0a3d944cbdad88f270552904bfbec
camilligan89 <67126551+camilligan89@users.noreply.github.com>
2021-12-15 20:58:40 -0500

    updated js

--- src/index.js
+++ src/index.js
@@ -72,7 +72,7 @@ function displayForecast(response) {
 
 function getForecast(coordinates) {
   let apiKey = "7fe508d235d0ee4554ec8bb21032d44e";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(displayForecast);
 }
 
